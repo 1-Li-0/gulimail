81a6030830ba4f32ed5b5ec992e4b73877fc3591
1-Li-0 <wy863405584@163.com>
2022-08-22 15:51:41 +0800

    逆向工程1

--- coupon/src/main/java/com/example/gulimall/coupon/controller/CouponController.java
+++ coupon/src/main/java/com/example/gulimall/coupon/controller/CouponController.java
@@ -5,6 +5,8 @@ import java.util.Map;
 
 //import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,16 +22,30 @@ import com.example.common.utils.R;
 
 /**
  * 优惠券信息
- *
- * @author Li
- * @email Li@163.com
- * @date 2022-08-18 15:26:31
+ *@RefreshScope 动态获取和刷新配置中心的配置信息
  */
+@RefreshScope
 @RestController
 @RequestMapping("coupon/coupon")
 public class CouponController {
     @Autowired
     private CouponService couponService;
+    @Value("${coupon.user.name}")
+    private String name;
+    @Value("${coupon.user.age}")
+    private String age;
+
+    @RequestMapping("/test")
+    public R test(){
+        return R.ok().put("name",name).put("age",age);
+    }
+
+    @RequestMapping("/member/list")
+    public R memberCoupons(){
+        CouponEntity couponEntity = new CouponEntity();
+        couponEntity.setCouponName("满100减10");
+        return R.ok().put("coupons",Arrays.asList(couponEntity));
+    }
 
     /**
      * 列表
---
+++ member/src/main/java/com/example/gulimall/member/feign/CouponFeignService.java
@@ -0,0 +1,13 @@
+package com.example.gulimall.member.feign;
+
+import com.example.common.utils.R;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@FeignClient("coupon")
+public interface CouponFeignService {
+
+    @RequestMapping("/coupon/coupon/member/list")
+    R memberCoupons();
+
+}
